98d62e0def6dbb86ff70d8b43c93cf97a910a433
sonam kumari <sonamjha869@gmail.com>
2022-08-23 12:36:35 +0530

    add my project

---
+++ src/com/javaproject3/ForLoop.java
@@ -0,0 +1,11 @@
+package com.javaproject3;
+
+public class ForLoop {
+	public static void main(String [] args) {
+		//using for loop in java program
+		for(int i= 1; i<=10; i++) {
+			System.out.println(i);
+		}
+	}
+
+}
